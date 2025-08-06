15e9eec4a835cb3942dd8004a2e2bf11bcb3d3d4
Alvaro vergara <alvarovergara1993@hotmail.com>
2025-08-05 23:56:18 -0500

    create table campaign and goal

---
+++ src/main/java/com/vaPaTi/vaPaTi/entity/Campaign.java
@@ -0,0 +1,46 @@
+package com.vaPaTi.vaPaTi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "campaign")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Campaign {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "goal_id")
+    private Goal goal;
+}
---
+++ src/main/java/com/vaPaTi/vaPaTi/entity/Goal.java
@@ -0,0 +1,38 @@
+package com.vaPaTi.vaPaTi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "goal")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Goal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "amount_goal", nullable = false, updatable = true)
+    private Double amountGoal;
+
+    @Column(name = "amount_raised", nullable = false, updatable = true)
+    private Double amountRaised;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
