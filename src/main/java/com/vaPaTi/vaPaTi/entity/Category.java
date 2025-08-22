20e25022e0f23ab32768d4dffbf0db69631fe1f7
Alvaro vergara <alvarovergara1993@hotmail.com>
2025-08-22 01:37:59 -0500

    fix: create builder in category

--- src/main/java/com/vaPaTi/vaPaTi/entity/Category.java
+++ src/main/java/com/vaPaTi/vaPaTi/entity/Category.java
@@ -1,10 +1,16 @@
 package com.vaPaTi.vaPaTi.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
