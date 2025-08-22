3f0220135bd4e4c62285d8d5bc545ef91e277cb7
Alvaro vergara <alvarovergara1993@hotmail.com>
2025-08-22 03:51:10 -0500

    fix: AuthenticationService

--- src/main/java/com/vaPaTi/vaPaTi/service/AuthenticationService.java
+++ src/main/java/com/vaPaTi/vaPaTi/service/AuthenticationService.java
@@ -23,9 +23,6 @@ public class AuthenticationService {
 
     @Transactional
     public AuthResponse authenticate(@NotNull AuthRequest request) {
-        if (request == null) {
-            throw new IllegalArgumentException("Authentication request cannot be null");
-        }
         
         try {
             // Use AuthenticationManager to validate credentials
