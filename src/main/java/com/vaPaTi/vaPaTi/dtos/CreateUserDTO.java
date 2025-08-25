61ad0b20ffb7d094cbfbea72947f5eb4c1bb3c11
Alvaro vergara <alvarovergara1993@hotmail.com>
2025-08-25 18:50:45 -0500

    Feat: add builder method

--- src/main/java/com/vaPaTi/vaPaTi/dtos/CreateUserDTO.java
+++ src/main/java/com/vaPaTi/vaPaTi/dtos/CreateUserDTO.java
@@ -1,11 +1,17 @@
 package com.vaPaTi.vaPaTi.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateUserDTO {
     private List<Long> categoryIds = new ArrayList<>();
 }
