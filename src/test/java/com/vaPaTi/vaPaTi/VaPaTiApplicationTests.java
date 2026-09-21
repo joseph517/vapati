567977ff469de6681409dba15849963e9d0a61fc
Alvaro Vergara <alvarovergara1993@hotmail.com>
2026-09-21 10:07:20 -0500

    Extend VaPaTiApplicationTests to inherit from AbstractIntegrationTest

--- src/test/java/com/vaPaTi/vaPaTi/VaPaTiApplicationTests.java
+++ src/test/java/com/vaPaTi/vaPaTi/VaPaTiApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.context.ApplicationContext;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-class VaPaTiApplicationTests {
+class VaPaTiApplicationTests extends AbstractIntegrationTest {
 
 	@Autowired
 	private ApplicationContext applicationContext;
