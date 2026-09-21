5a53031b086f2bd16711cca20e5a0b9ad990ebf5
Alvaro Vergara <alvarovergara1993@hotmail.com>
2026-09-21 09:03:53 -0500

    Add method to find campaigns by goal status in CampaignRepository

--- src/main/java/com/vaPaTi/vaPaTi/repository/CampaignRepository.java
+++ src/main/java/com/vaPaTi/vaPaTi/repository/CampaignRepository.java
@@ -1,6 +1,7 @@
 package com.vaPaTi.vaPaTi.repository;
 
 import com.vaPaTi.vaPaTi.entity.Campaign;
+import com.vaPaTi.vaPaTi.entity.CampaignStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,7 @@ import java.util.List;
 @Repository
 public interface CampaignRepository extends JpaRepository<Campaign, Long> {
     List<Campaign> findByUserId(Long userId);
+
+    List<Campaign> findByGoal_Status(CampaignStatus status);
 }
 
